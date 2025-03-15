fd25c2f5d1d8fa6e97f0fe87c629b5fe7a39a86d
Axel Eldrian <a.hadiwibowo@moovd.nl>
2025-03-15 19:49:03 +0700

    feat(): init require file

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const connectDB =  require('./db');
+const Product = require('./product');
 
 const app = express();
 app.use(express.json());
 
-const port = 5000;
+const port = process.env.PORT || 5001;
 
 app.listen(port, () => {
     console.log("api server started on port 5000");
